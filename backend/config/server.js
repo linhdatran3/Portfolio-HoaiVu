2df3f4324d3873de03da52ea477b059195bc89e6
Nguyen Thanh Tien <tiennt0212@gmail.com>
2022-01-17 00:34:53 +0700

    init :deploy: BE side

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9fd932c3f3fd2108b37b3b5604741866'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '853b5303-de6e-4164-bc3c-a2b6dac33aba'
+};
