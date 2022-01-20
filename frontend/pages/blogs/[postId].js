42dd37bee0167cc9340be30976c326b97a95d0be
Nguyen Thanh Tien <tiennt0212@gmail.com>
2022-01-20 21:57:04 +0700

    feat :zap: Post Detail component

--- frontend/pages/blogs/[postId].js
+++ frontend/pages/blogs/[postId].js
@@ -1,8 +1,14 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { PostDetail } from "../../components/PostDetail";
+import { Container } from "@mui/material";
 
 const BlogDetailPage = () => {
-  return <Layout>Blog detail</Layout>;
+  return (
+    <Layout>
+      <PostDetail />
+    </Layout>
+  );
 };
 
 export default BlogDetailPage;
