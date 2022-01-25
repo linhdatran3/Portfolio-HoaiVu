fa8d53f49747428a472e20c31f87e3dbfefd2751
linhdatran3 <linhdatran3@gmail.com>
2022-01-26 00:03:00 +0700

    refactor :zap: FE prints & service page

--- frontend/pages/prints.js
+++ frontend/pages/prints.js
@@ -5,17 +5,46 @@ import { ContactForm } from "../components/ContactForm";
 import Image from "next/image";
 import fine_print from "./../public/fine-print.jpg";
 import price_print from "./../public/price-print.png";
-import styles from "../styles/Home.module.css";
-
+import stylesPrint from "../styles/Print.module.css";
 const Prints = () => {
   return (
-    <Layout className={styles.Container}>
-      <Title title="FINE & PRINT" />
-      <div className="image" style={{ display: "flex" }}>
-        <Image src={price_print} alt="price" />
-        <Image src={fine_print} alt="fine and print" />
+    <Layout>
+      <div className={stylesPrint.fine__print}>
+        <Title title="FINE & PRINT" />
+        <div className={stylesPrint.content}>
+          <div className={stylesPrint.price}>
+            <p>
+              We provide top-quality art prints. Each painting is printed with
+              high-quality materials that are durable over time, and we make the
+              most meticulous work.{" "}
+            </p>
+            <p>
+              Below is the reference price list of my art prints, other than
+              these sizes you can contact me. Please note that this price list
+              does not include the photo frame or shipping fee.
+            </p>
+            <div className={stylesPrint.image__price}>
+              <Image src={price_print} alt="price" />
+            </div>
+
+            <p>
+              Please contact me via the mailbox below if you are interested in
+              some of my photos.
+            </p>
+            <p>
+              All my works do not exist only on this website. Let me know what
+              you intend to use your print for so I can advise you, in addition
+              to art prints it could be for advertising purposes.
+            </p>
+          </div>
+          <div className={stylesPrint.fine}>
+            <Image src={fine_print} alt="fine and print" />
+          </div>
+        </div>
       </div>
-      <ContactForm />
+      <div className={stylesPrint.contact}>
+        <ContactForm />
+      </div>
     </Layout>
   );
 };
--- frontend/pages/services.js
+++ frontend/pages/services.js
@@ -1,12 +1,46 @@
 import React from "react";
+import Masonry from "../components/ImageMasonry";
 import { ContactForm } from "../components/ContactForm";
 import Layout from "../components/Layout";
 import Title from "../components/Title";
+import stylesService from "../styles/Service.module.css";
 const Services = () => {
   return (
     <Layout>
-      <Title title="FINE ART PRINTS" />
-      <ContactForm />
+      <div className={stylesService.service}>
+        <Title title="FINE ART PRINTS" />
+        <div className={stylesService.content}>
+          <div className={stylesService.script__1}>
+            <p>
+              In this increasingly technologically advanced age, I understand
+              very well the important role visuals play in your business.
+            </p>
+            <p>
+              You can find in me a prefessional photography service, I
+              specialize in photo projects for hotels, restaurants, resorts, ...
+            </p>
+            <p>
+              {" "}
+              Please contact me when you need a service to provide professional
+              images for your project.
+            </p>
+          </div>
+          <div className={stylesService.script__2}>
+            <p>
+              Below is a sample project that I have done for a hotel, contact me
+              via email so I can know the size of your project so I can give you
+              an exact price.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className={stylesService.masonry}>
+        <Masonry />
+      </div>
+      <div className={stylesService.contact}>
+        <ContactForm />
+      </div>
     </Layout>
   );
 };
